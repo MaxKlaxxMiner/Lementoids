3da6f64dd66b0d4769b4c39cbdb4760f2c93aa6e
MaxKlaxx <max@tetrisk.de>
2018-08-04 16:34:04 +0200

    Projekt hinzugefügt: LemTest

---
+++ LemTest/Program.cs
@@ -0,0 +1,22 @@
+﻿#region # using *.*
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+#endregion
+
+namespace LemTest
+{
+  static class Program
+  {
+    static void Main()
+    {
+      if (Environment.CommandLine.Contains(".vshost.exe"))
+      {
+        Console.WriteLine("Press any key to continue . . . ");
+        Console.ReadKey(true);
+      }
+    }
+  }
+}
