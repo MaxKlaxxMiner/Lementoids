76e054e8967fae6467ea48247b53e04e7b78c992
MaxKlaxx <max@tetrisk.de>
2018-08-04 17:05:49 +0200

    LemTool: Gradient-Beispiel hinzugefügt

--- LemTool/LemToolForm1.Designer.cs
+++ LemTool/LemToolForm1.Designer.cs
@@ -28,20 +28,36 @@
     /// </summary>
     private void InitializeComponent()
     {
+      this.gradientBox1 = new System.Windows.Forms.PictureBox();
+      ((System.ComponentModel.ISupportInitialize)(this.gradientBox1)).BeginInit();
       this.SuspendLayout();
+      // 
+      // gradientBox1
+      // 
+      this.gradientBox1.Location = new System.Drawing.Point(12, 12);
+      this.gradientBox1.Name = "gradientBox1";
+      this.gradientBox1.Size = new System.Drawing.Size(1024, 50);
+      this.gradientBox1.TabIndex = 0;
+      this.gradientBox1.TabStop = false;
       // 
       // LemToolForm1
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(1191, 660);
+      this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+      this.ClientSize = new System.Drawing.Size(1235, 660);
+      this.Controls.Add(this.gradientBox1);
       this.Name = "LemToolForm1";
       this.Text = "Lementoids Tool";
+      this.Shown += new System.EventHandler(this.LemToolForm1_Shown);
+      ((System.ComponentModel.ISupportInitialize)(this.gradientBox1)).EndInit();
       this.ResumeLayout(false);
 
     }
 
     #endregion
+
+    private System.Windows.Forms.PictureBox gradientBox1;
   }
 }
 
--- LemTool/LemToolForm1.cs
+++ LemTool/LemToolForm1.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +17,36 @@ namespace LemTool
     public LemToolForm1()
     {
       InitializeComponent();
+    }
+
+    private void LemToolForm1_Shown(object sender, EventArgs e)
+    {
+      var gColor = new int[1024];
+      for (int i = 0; i < 256; i++)
+      {
+        gColor[i] = i;
+        gColor[i + 256] = (255 - i) + i * 256;
+        gColor[i + 512] = (255 - i) * 256 + i * 65536;
+        gColor[i + 768] = i + i * 256 + 255 * 65536;
+      }
+
+      var gradient = new Bitmap(1024, gradientBox1.Height, PixelFormat.Format32bppRgb);
+      int width = gradient.Width;
+      int height = gradient.Height;
+      var gradientPix = new int[width * height];
+
+      for (int y = 0; y < height; y++)
+      {
+        for (int x = 0; x < width; x++)
+        {
+          gradientPix[x + y * width] = gColor[x];
+        }
+      }
+
+      var bdata = gradient.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.WriteOnly, PixelFormat.Format32bppRgb);
+      Marshal.Copy(gradientPix, 0, bdata.Scan0, gradientPix.Length);
+      gradient.UnlockBits(bdata);
+      gradientBox1.Image = gradient;
     }
   }
 }
