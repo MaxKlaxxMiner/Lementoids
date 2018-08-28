54d03127100327be0b7eb882303b1cc455a2cbbe
MaxKlaxxMiner <max@tetrisk.de>
2018-08-28 20:03:10 +0200

    MultiColors als Farbverlauf hinzugefügt

--- LemTool/LemToolForm1.cs
+++ LemTool/LemToolForm1.cs
@@ -18,83 +18,30 @@ namespace LemTool
       InitializeComponent();
     }
 
-    static int[] GradientMap(Dictionary<int, Color3F> colorsDict)
-    {
-      var colors = colorsDict.OrderBy(x => x.Key).ToArray();
-      int min = colors.Min(x => x.Key);
-      int max = colors.Max(x => x.Key);
-      var output = new int[max - min + 1];
+    bool multiColor = true;
 
-      var lastC = colors.First().Value;
-      int lastP = colors.First().Key;
-      foreach (var c in colors.Skip(1))
-      {
-        for (int p = lastP; p < c.Key; p++)
-        {
-          output[p - min] = Color3F.Mix(lastC, c.Value, 1f / (c.Key - lastP) * (p - lastP)).Int32;
-        }
-        lastP = c.Key;
-        lastC = c.Value;
-      }
-      output[output.Length - 1] = colors.Last().Value.Int32;
-      return output;
+    void UpdatePic()
+    {
+      pictureBox1.Image = TestTools.GradientTest(pictureBox1.Width, pictureBox1.Height, multiColor);
     }
 
     private void LemToolForm1_Shown(object sender, EventArgs e)
     {
-      var colors = new Dictionary<int, Color3F>
+      UpdatePic();
+    }
+
+    private void LemToolForm1_Resize(object sender, EventArgs e)
+    {
+      UpdatePic();
+    }
+
+    private void LemToolForm1_KeyPress(object sender, KeyPressEventArgs e)
+    {
+      if (e.KeyChar == ' ')
       {
-        { 0, new Color3F(0, 0, 0) }, // black
-        { 255, new Color3F(0, 0, 1) }, // blue
-        { 511, new Color3F(0, 1, 0) }, // gree
-        { 767, new Color3F(1, 0, 0) }, // red
-        { 1023, new Color3F(1, 1, 1) } // white
-      };
-
-      var test = GradientMap(colors);
-
-      var c0 = new Color3F(0, 0, 0); // black
-      var c1 = new Color3F(0, 0, 1); // blue
-      var c2 = new Color3F(0, 1, 0); // gree
-      var c3 = new Color3F(1, 0, 0); // red
-      var c4 = new Color3F(1, 1, 1); // white
-
-      var gColor = new int[1024];
-      for (int i = 0; i < 256; i++)
-      {
-        float f = i / 256f;
-
-        gColor[i] = Color3F.Mix(c0, c1, f).Int32;
-        gColor[i + 256] = Color3F.Mix(c1, c2, f).Int32;
-        gColor[i + 512] = Color3F.Mix(c2, c3, f).Int32;
-        gColor[i + 768] = Color3F.Mix(c3, c4, f).Int32;
+        multiColor = !multiColor;
+        UpdatePic();
       }
-
-      var gradient = new Bitmap(1024, gradientBox1.Height, PixelFormat.Format32bppRgb);
-      int width = gradient.Width;
-      int height = gradient.Height;
-      var gradientPix = new int[width * height];
-
-      for (int y = 0; y < height / 2; y++)
-      {
-        for (int x = 0; x < width; x++)
-        {
-          gradientPix[x + y * width] = gColor[x];
-        }
-      }
-
-      for (int y = height / 2 + 1; y < height; y++)
-      {
-        for (int x = 0; x < width; x++)
-        {
-          gradientPix[x + y * width] = test[x];
-        }
-      }
-
-      var bdata = gradient.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.WriteOnly, PixelFormat.Format32bppRgb);
-      Marshal.Copy(gradientPix, 0, bdata.Scan0, gradientPix.Length);
-      gradient.UnlockBits(bdata);
-      gradientBox1.Image = gradient;
     }
   }
 }
---
+++ LemTool/TestTools/TestTools.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using LemLib;
+
+namespace LemTool
+{
+  public static partial class TestTools
+  {
+    static int[] GradientMap(Dictionary<int, Color3F> colorsDict)
+    {
+      var colors = colorsDict.OrderBy(x => x.Key).ToArray();
+      int min = colors.Min(x => x.Key);
+      int max = colors.Max(x => x.Key);
+      var output = new int[max - min + 1];
+
+      var lastC = colors.First().Value;
+      int lastP = colors.First().Key;
+      foreach (var c in colors.Skip(1))
+      {
+        for (int p = lastP; p < c.Key; p++)
+        {
+          output[p - min] = Color3F.Mix(lastC, c.Value, 1f / (c.Key - lastP) * (p - lastP)).Int32;
+        }
+        lastP = c.Key;
+        lastC = c.Value;
+      }
+      output[output.Length - 1] = colors.Last().Value.Int32;
+      return output;
+    }
+
+    public static Bitmap GradientTest(int width, int height, bool multiColor)
+    {
+      var colors = new Dictionary<int, Color3F>
+      {
+        { 0, new Color3F(0, 0, 0) }, // black
+        { 255, new Color3F(0, 0, 1) }, // blue
+        { 511, new Color3F(0, 1, 0) }, // gree
+        { 767, new Color3F(1, 0, 0) }, // red
+        { 1023, new Color3F(1, 1, 1) } // white
+      };
+
+      var gradientMap = GradientMap(colors);
+
+      var gradientTest = new Bitmap(width, height, PixelFormat.Format32bppRgb);
+      var gradientPix = new int[width * height];
+
+      if (multiColor)
+      {
+        var black = new Color3F(0x000000);
+        var white = new Color3F(0xffffff);
+        for (int x = 0; x < width; x++)
+        {
+          int pos = x * 1024 / width;
+          var gColor = new Color3F(gradientMap[pos]);
+          for (int y = 0; y < height / 2; y++)
+          {
+            gradientPix[x + y * width] = Color3F.Mix(black, gColor, y / (height / 2f)).Int32;
+          }
+          for (int y = height / 2; y < height; y++)
+          {
+            gradientPix[x + y * width] = Color3F.Mix(gColor, white, (y - height / 2) / (height / 2f)).Int32;
+          }
+        }
+      }
+      else
+      {
+        for (int x = 0; x < width; x++)
+        {
+          int pos = x * 1024 / width;
+          var gColor = gradientMap[pos];
+          for (int y = 0; y < height; y++)
+          {
+            gradientPix[x + y * width] = gColor;
+          }
+        }
+      }
+
+      var bdata = gradientTest.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.WriteOnly, PixelFormat.Format32bppRgb);
+      Marshal.Copy(gradientPix, 0, bdata.Scan0, gradientPix.Length);
+      gradientTest.UnlockBits(bdata);
+
+      return gradientTest;
+    }
+  }
+}
